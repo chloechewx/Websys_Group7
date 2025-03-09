374f25e157f70f94c326b4f8b204c7bc041af34a
Chloechew <105104684+chloechewx@users.noreply.github.com>
2025-03-09 21:09:00 +0800

    updated landingpage

--- index.php
+++ index.php
@@ -7,23 +7,17 @@
     </head>
 
 <body>
-<header class="site-header">
-    <div class="header-bg">
-        <img src="images/placeholder.jpg" alt="Header background" class="header-bg-img">
-    </div>
     <?php
     include "inc/navbar.inc.php";
-    include "inc/header.inc.php";
     ?>
-</header>
 
-//working atm..
-    //<main class="container">
-   // <?php
-  //  include "inc/featured-products.inc.php";
-  //  include "inc/lifestyle-section.inc.php";
-   // ?>
-   // </main>
+
+    <?php
+    include "inc/featured-products.inc.php";
+    include "inc/lifestyle-section.inc.php";
+    ?>
+
+
 
 <?php
 include "inc/footer.inc.php";
