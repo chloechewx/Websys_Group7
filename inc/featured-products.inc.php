2a77ba6aac3606c4b165a41172146bceaf190a13
Chloechew <105104684+chloechewx@users.noreply.github.com>
2025-03-09 21:10:39 +0800

    added 2 more section to landpage

---
+++ inc/featured-products.inc.php
@@ -0,0 +1,48 @@
+<section class="featured-products">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <h1 class="featured-title mb-4">Featured Products</h1>
+                <div class="mb-4">
+                    <a href="products.php" class="btn btn-outline-dark rounded-pill shop-now-btn">View All</a>
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="row">
+            <!-- Product 1 -->
+            <div class="col-12 col-lg-4 d-flex justify-content-center mb-4">
+                <div class="product-card position-relative text-center" style="width: 40%;">
+                    <img src="images/bonsainobg.png" alt="Bonsai">
+                    <div class="product-info text-center" style="margin-top: 60px;">
+                        <h2 class="product-name">Bonsai</h2>
+                        <p class="product-price">from SGD 88.00</p>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- Product 2 -->
+            <div class="col-12 col-lg-4 d-flex justify-content-center mb-4">
+                <div class="product-card position-relative text-center" style="width: 40%;">
+                    <img src="images/fiddle-leaf-fignobg.png" alt="Fiddle Leaf Fig">
+                    <div class="product-info text-center"style="margin-top: 60px;">
+                        <h2 class="product-name">Fiddle Leaf Fig</h2>
+                        <p class="product-price">SGD 28.00</p>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- Product 3 -->
+            <div class="col-12 col-lg-4 d-flex justify-content-center mb-4">
+                <div class="product-card position-relative text-center" style="width: 40%;">
+                    <img src="images/succulentnobg.png" alt="Succulent">
+                    <div class="product-info text-center"style="margin-top: 60px;">
+                        <h2 class="product-name">Succulent</h2>
+                        <p class="product-price">from SGD 18.00</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ inc/lifestyle-section.inc.php
@@ -0,0 +1,12 @@
+<section class="lifestyle-section">
+    <div class="container py-5">
+        <div class="lifestyle-image position-relative">
+            <img src="images/lifestylesection.jpg" class="w-100" alt="Tableware">
+            <!-- added overlay effect to look less dead -->
+            <div class="lifestyle-content position-absolute bottom-0 p-4 text-white">
+                <h3 class="lifestyle-title">Tableware - Handcrafted in Japan</h3>
+                <a href="lifestyle.php" class="btn btn-outline-light rounded-pill shop-now-btn">Shop now</a>
+            </div>
+        </div>
+    </div>
+</section>
